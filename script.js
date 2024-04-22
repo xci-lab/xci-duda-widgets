658e25f03f8ad07ffba7eb015bdb4f116c049e5f
xci-Lab <105429449+xci-lab@users.noreply.github.com>
2024-04-22 14:17:23 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+  el: "#myContainer",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  points: 12.00,
+  maxDistance: 22.00
+})
